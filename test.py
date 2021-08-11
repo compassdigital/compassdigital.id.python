e5eb359300153817b7b182150d3e92f29c8170f5
Ilia Choly <ilia.choly@compassdigital.io>
2021-08-11 11:56:01 -0400

    add tests

---
+++ test.py
@@ -0,0 +1,39 @@
+import unittest
+import cdlid
+
+expected_ids = [
+    ["eW0gy25y3BFaYvgPlYgmfPmZloM3d2fE032BvlZDH2g5r6qkYau18BQgWGOGFAQMvl03NkHX80", {
+        "service": "location",
+        "provider": "cdl",
+        "type": "group",
+        "id": "7c732f634cbf443092ae6f289d80121f",
+    }],
+    ["11J3gKPg8BCR3mr5OO92S6EBL4ddEAT17G44eoLPSw0N21gy4OHjQXjDG6LXIrL1MY8B5PHPX9omNMrqFJO", {
+        "service": "location",
+        "provider": "cdl",
+        "type": "multigroup",
+        "id": "ec65915647e84bcd83f536ad29c694d2"
+    }],
+    ["MX85pE82L4sLvrXkw3A2TyaYyemGXQi1JkME8J1GiLBRjBzE8vH1Dzevm1WefD2eRvlXOeFKadpMoEvRtX49Z", {
+        "service": "user",
+        "provider": "cdl",
+        "type": "user",
+        "id": "cdl/f6d6816279e64b85bb2670cf04d5dfce"
+    }],
+    ["9PD8DyAg2RFZJ3yoDLYJiapBl0yr71hDZN", {
+        "service": "menu",
+        "provider": "bamco",
+        "type": "item",
+        "id": "1234",
+    }]
+]
+
+class TestEncodingDecodingID(unittest.TestCase):
+    def test_encode_decode(self):
+        for encoded, decoded in expected_ids:
+            print(encoded)
+            self.assertDictEqual(decoded, cdlid.decode(encoded))
+            self.assertEqual(encoded, cdlid.encode(decoded))
+
+if __name__ == "__main__":
+    unittest.main()
